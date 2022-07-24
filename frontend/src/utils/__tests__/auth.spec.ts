e96d74e975b8bdef572e8cc860255239edbd86b1
auriceliof <auricelio.suporte@hotmail.com>
2022-07-23 21:37:25 -0300

    Test auth

---
+++ frontend/src/utils/__tests__/auth.spec.ts
@@ -0,0 +1,11 @@
+import { hasAnyRoles } from "utils/auth";
+
+describe('hasAnyRoles tests', () => {
+
+    test('should return true when empty list', () => {
+
+        const result = hasAnyRoles([]);
+        expect(result).toEqual(true);
+
+    });
+});
