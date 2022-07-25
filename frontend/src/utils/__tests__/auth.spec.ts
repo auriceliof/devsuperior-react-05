47fbee75fb0b584014a13b00a50770d2dd96d597
auriceliof <auricelio.suporte@hotmail.com>
2022-07-25 19:53:25 -0300

    Testing another hasAnyRoles scenario

--- frontend/src/utils/__tests__/auth.spec.ts
+++ frontend/src/utils/__tests__/auth.spec.ts
@@ -3,12 +3,14 @@ import * as TokenModule from "../token"
 
 describe('hasAnyRoles tests', () => {
 
+    // CENARIO 1
     test('should return true when empty list', () => {
         const result = hasAnyRoles([]);
         expect(result).toEqual(true);
     });
 
-    test.only('should return true when user has given role', () => {
+    // CENARIO 2
+    test('should return true when user has given role', () => {
 
         jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
             exp: 0,
@@ -18,5 +20,18 @@ describe('hasAnyRoles tests', () => {
 
         const result = hasAnyRoles(['ROLE_ADMIN']);
         expect(result).toEqual(true);
+    });
+
+     // CENARIO 3
+     test('should return false when user does not have given role', () => {
+
+        jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+            exp: 0,
+            user_name: '',
+            authorities: ['ROLE_OPERATOR'],
+        }); 
+
+        const result = hasAnyRoles(['ROLE_ADMIN']);
+        expect(result).toEqual(false);
     });
 });
