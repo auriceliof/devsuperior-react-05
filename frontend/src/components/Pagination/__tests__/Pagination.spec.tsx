2d2f7ee3d426b0b09b4de18e8bcaf0f30f4f6286
auriceliof <auricelio.suporte@hotmail.com>
2022-07-27 17:02:59 -0300

    Call onChange test and rollback user-event 13

--- frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
+++ frontend/src/components/Pagination/__tests__/Pagination.spec.tsx
@@ -1,12 +1,13 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Pagination from "..";
 
 describe('Pagination tests', () => {
     test( 'should render Pagination', () => {
         
         const pageCount = 3;
-        const range = 3
-        
+        const range = 3;
+                
         render(
             <Pagination 
                 pageCount={pageCount}
@@ -29,6 +30,28 @@ describe('Pagination tests', () => {
         expect(page3).not.toHaveClass("pagination-link-active");
 
         expect(page4).not.toBeInTheDocument();
+
+    });
+
+    test( 'next arrow should call onChange', () => {
+        
+        const pageCount = 3;
+        const range = 3;
+        const onChange = jest.fn();
+        
+        render(
+            <Pagination 
+                pageCount={pageCount}
+                range={range}
+                onChange={onChange}            
+            />
+        );
+
+        const arrowNext = screen.getByTestId("arrow-next");
+
+        userEvent.click(arrowNext);
+
+        expect(onChange).toHaveBeenCalledWith(1);
 
     });
 });
