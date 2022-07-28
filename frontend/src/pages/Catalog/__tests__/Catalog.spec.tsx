812d361d063a8ada81c5f40b1f36ba48f3224bcc
auriceliof <auricelio.suporte@hotmail.com>
2022-07-28 14:49:56 -0300

    Catalog, waitFor, async-await

--- frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontend/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import history from 'utils/history';
 import Catalog from "..";
@@ -6,7 +6,7 @@ import Catalog from "..";
 describe( 'should render Catalog with products', () => {
 
     //CENÁRIO-1
-    test( '', () => {
+    test( 'should render Catalog', () => {
     
         render(
             <Router history={history}>
@@ -15,6 +15,23 @@ describe( 'should render Catalog with products', () => {
         );
 
         expect(screen.getByText('Catálogo de Produtos')).toBeInTheDocument();
-        
+
     });    
+
+    //CENÁRIO-2
+    test( 'should render Product async', async () => {
+    
+        render(
+            <Router history={history}>
+                <Catalog />
+            </Router>
+        );
+      
+        expect(screen.getByText('Catálogo de Produtos')).toBeInTheDocument();
+
+        await waitFor(() => {
+            expect(screen.getByText('Smart TV')).toBeInTheDocument();
+        });
+    });
+        
 });
