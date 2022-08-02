8e7441927e4070cc0844ca6121d7540ad0e20ae7
auriceliof <auricelio.suporte@hotmail.com>
2022-08-02 09:48:07 -0300

    Mocking react-router-dom

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,16 +1,29 @@
 import { render, screen } from "@testing-library/react";
-import { Router } from "react-router-dom";
+import { Router, useParams } from "react-router-dom";
 import history from 'utils/history'
 import Form from "../Form";
 
-test( 'Should render form', () => {
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: jest.fn()
+}));
 
-    render(
-        <Router history={history}>
-            <Form />
-        </Router>
-    );
+describe('Product form create tests', () => {
 
-    screen.debug();
-});
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: 'create'
+        })
+    })
 
+    test( 'Should render form', () => {
+    
+        render(
+            <Router history={history}>
+                <Form />
+            </Router>
+        );
+    
+        screen.debug();
+    });            
+}); 
