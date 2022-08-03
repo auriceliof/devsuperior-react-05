6c9731ab08262ab80d0dd11616e1ee1f0e282611
auriceliof <auricelio.suporte@hotmail.com>
2022-08-03 11:38:43 -0300

    Simulate typing

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,7 +1,10 @@
 import { render, screen } from "@testing-library/react";
 import { Router, useParams } from "react-router-dom";
-import history from 'utils/history'
+import history from 'utils/history';
 import Form from "../Form";
+import useEvent from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 
 jest.mock('react-router-dom', () => ({
     ...jest.requireActual('react-router-dom'),
@@ -31,5 +34,10 @@ describe('Product form create tests', () => {
         const imgUrlInput = screen.getByTestId("imgUrl");
         const descriptionInput = screen.getByTestId("description");
         const categoriesInput = screen.getByLabelText("Categorias");
+
+        userEvent.type(nameInput, 'Computador');
+        userEvent.type(priceInput, '5000.12');
+        userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg');
+        userEvent.type(descriptionInput, 'Computador muito bom');
     });            
 }); 
