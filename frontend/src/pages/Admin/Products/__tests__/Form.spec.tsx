eba560eb9424bf14d5b94bc629a7ca5791c3a66b
auriceliof <auricelio.suporte@hotmail.com>
2022-08-04 10:51:35 -0300

    Simulate Toast exhibition

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -1,10 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { Router, useParams } from "react-router-dom";
 import history from 'utils/history';
 import Form from "../Form";
 import userEvent from "@testing-library/user-event";
 import { server } from "./fixtures";
 import selectEvent from "react-select-event";
+import { ToastContainer } from "react-toastify";
 
 beforeAll(() => server.listen());
 afterEach(() => server.restoreHandlers());
@@ -27,6 +28,7 @@ describe('Product form create tests', () => {
     
         render(
             <Router history={history}>
+                <ToastContainer />
                 <Form />
             </Router>
         );
@@ -39,11 +41,20 @@ describe('Product form create tests', () => {
         const descriptionInput = screen.getByTestId("description");
         const categoriesInput = screen.getByLabelText("Categorias");
 
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores'])
+
         userEvent.type(nameInput, 'Computador');
         userEvent.type(priceInput, '5000.12');
         userEvent.type(imgUrlInput, 'https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/4-big.jpg');
         userEvent.type(descriptionInput, 'Computador muito bom');
 
-        await selectEvent.select(categoriesInput, ['Eletrônicos', 'Computadores'])
+        userEvent.click(submitButton);
+        
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso')
+                expect(toastElement).toBeInTheDocument();
+        });
     });            
 }); 
