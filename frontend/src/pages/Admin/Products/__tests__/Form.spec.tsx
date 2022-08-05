10a15170db7278b2b9cacb869a57a5631bdce2b4
auriceliof <auricelio.suporte@hotmail.com>
2022-08-05 14:56:13 -0300

    Test Should show toast and redirect when form correctly PART-1

--- frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
+++ frontend/src/pages/Admin/Products/__tests__/Form.spec.tsx
@@ -3,7 +3,7 @@ import { Router, useParams } from "react-router-dom";
 import history from 'utils/history';
 import Form from "../Form";
 import userEvent from "@testing-library/user-event";
-import { server } from "./fixtures";
+import { productResponse, server } from "./fixtures";
 import selectEvent from "react-select-event";
 import { ToastContainer } from "react-toastify";
 
@@ -16,7 +16,7 @@ jest.mock('react-router-dom', () => ({
     useParams: jest.fn()
 }));
 
-describe('Product form create tests', () => {
+describe('Product form CREATE tests', () => {
 
     beforeEach(() => {
         (useParams as jest.Mock).mockReturnValue({
@@ -121,3 +121,48 @@ describe('Product form create tests', () => {
         });
     });  
 }); 
+
+//---------------------------------------------------------------------------
+
+describe('Product form UPDATE tests', () => {
+
+    beforeEach(() => {
+        (useParams as jest.Mock).mockReturnValue({
+            productId: '2'
+        })
+    })
+
+    test( 'Should show toast and redirect when form correctly', async() => {
+        
+        render(
+            <Router history={history}>
+                <ToastContainer />
+                <Form />
+            </Router>
+        );
+    
+        await waitFor (() => {
+            const nameInput = screen.getByTestId("name");
+            const priceInput = screen.getByTestId("price");
+            const imgUrlInput = screen.getByTestId("imgUrl");
+            const descriptionInput = screen.getByTestId("description");
+    
+            expect(nameInput).toHaveValue(productResponse.name);
+            expect(priceInput).toHaveValue(String(productResponse.price));
+            expect(imgUrlInput).toHaveValue(productResponse.imgUrl);
+            expect(descriptionInput).toHaveValue(productResponse.description);
+        });    
+
+        const submitButton = screen.getByRole('button', { name: /salvar/i });
+
+        userEvent.click(submitButton);
+
+        await waitFor(() => {
+            const toastElement = screen.getByText('Produto cadastrado com sucesso')
+                expect(toastElement).toBeInTheDocument();
+        });
+
+        expect(history.location.pathname).toEqual('/admin/products');       
+    });
+});
+
